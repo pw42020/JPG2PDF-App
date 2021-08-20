0cfc364b9b21cdfb6264561084a3c271190ebbf7
pw42020 <pw4.2020@gmail.com>
2021-08-20 19:16:36 -0400

    Created add document function
    Can copy files from anywhere on your computer into the documents folder in Kivy.

--- Kivy/Untitled-1.py
+++ Kivy/Untitled-1.py
@@ -5,17 +5,36 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
-class MyGrid(Widget):
-    pass
+from plyer import filechooser
+import os
+from shutil import copy
+
+class MyWidget(Widget):
+    
+    def findfile(self):
+        path = str(filechooser.open_file(title="Pick an image or document",path=r"C:\Users\Ninja\OneDrive\Desktop"))
+        path=path.replace("[","")
+        path=path.replace("]","")
+        path=path.replace("'","")
+        copy(path,r"C:\Users\Ninja\OneDrive\Documents\GitHub\App\Documents")
+        
+
+
+    def selected(self,file):
+        try:
+            self.ids.image.source=filename[0]
+        except:
+            pass
 
 
 
-class MyApp(App):
+class FileChooserWindow(App):
     def build(self):
-        return MyGrid()
+
+        return MyWidget()
 
 
 
 
 if __name__=="__main__":
-    MyApp().run()
+    FileChooserWindow().run()
