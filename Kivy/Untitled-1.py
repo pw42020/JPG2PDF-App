f1f423e24c1e50106bad55347eee5e82b60d2719
pw42020 <pw4.2020@gmail.com>
2021-08-22 16:01:41 -0400

    Worked on toolbar and screen functionality
    Added two screens, one for documents and one for menu. Will most likely change in future.

--- Kivy/Untitled-1.py
+++ Kivy/Untitled-1.py
@@ -1,16 +1,20 @@
-from kivy.app import App
-from kivy.uix.label import Label 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivymd.app import MDApp
+from kivy.uix.filechooser import FileChooserIconView
+from kivy.core.window import Window
+
 from kivy.uix.widget import Widget
 
+from kivy.lang.builder import Builder
+
 from plyer import filechooser
-import os
 from shutil import copy
 
-class MyWidget(Widget):
-    
+from kivy.uix.screenmanager import Screen,ScreenManager
+
+Window.size=(300,500) ######MAKE SURE TO REMOVE LATER ######
+
+
+class MenuScreen(Screen):
     def findfile(self):
         path = str(filechooser.open_file(title="Pick an image or document",path=r"C:\Users\Ninja\OneDrive\Desktop"))
         path=path.replace("[","")
@@ -27,14 +31,38 @@ class MyWidget(Widget):
             pass
 
 
+class DocumentsScreen(Screen):
+    pass
 
-class FileChooserWindow(App):
+class WindowManager(ScreenManager):
+    pass
+
+
+
+class CreatePDF(MDApp):
     def build(self):
-
-        return MyWidget()
+        kv=Builder.load_file("createpdf.kv")
+        return kv
+    def navigation_draw(self):
+        print("Navigation")
+    
+    
 
 
 
 
 if __name__=="__main__":
-    FileChooserWindow().run()
+    CreatePDF().run()
+
+"""
+GridLayout:
+        cols:1
+        size: root.width,root.height*0.2
+        Button:
+            text:"Add Document"
+            on_press:root.findfile()
+        Button:
+            text:"Documents"
+            on_press:
+                root.manager.transition.direction="left"
+                app.root.current="documents"""
