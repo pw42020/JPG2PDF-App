782c96aedb13cd3ddfcd2602f401b4cc307b8198
pw42020 <pw4.2020@gmail.com>
2021-08-23 21:48:50 -0400

    Lots of changes (SEE BELOW)
    Removed documents screen, added all the buttons on the pullout menu and added a button to add documents. At the moment no buttons have uses.

--- Kivy/Untitled-1.py
+++ Kivy/Untitled-1.py
@@ -1,15 +1,10 @@
 from types import BuiltinFunctionType
 from kivymd.app import MDApp
-from kivy.uix.filechooser import FileChooserIconView
 from kivy.core.window import Window
 from kivymd.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
 from kivymd.uix.menu import MDDropdownMenu
-from kivymd.uix.list import OneLineIconListItem,MDList
-from kivy.properties import StringProperty
-from kivymd.theming import ThemableBehavior
-
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
 
 from plyer import filechooser
 from shutil import copy
@@ -32,61 +27,40 @@ class WindowManager(ScreenManager):
 class ContentNavigationDrawer(BoxLayout):
     pass
 
-class ItemDrawer(OneLineIconListItem):
-    icon = StringProperty()
 
-class DrawerList(ThemableBehavior, MDList):
-    def set_color_item(self, instance_item):
-        '''Called when tap on a menu item.'''
 
-        # Set the color of the icon and text for the menu item.
-        for item in self.children:
-            if item.text_color == self.theme_cls.primary_color:
-                item.text_color = self.theme_cls.text_color
-                break
-        instance_item.text_color = self.theme_cls.primary_color
 
 class CreatePDF(MDApp):
+    menu=ObjectProperty()
     def build(self):
-        kv=Builder.load_file("createpdf.kv")
-        menu_items = [
-            {
-                "text": f"Item {i}",
-                "viewclass": "OneLineListItem",
-                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
-            } for i in range(5)
-        ]
-        self.menu = MDDropdownMenu(
-
-            items=menu_items,
-            width_mult=4,
-        )
+        self.kv=Builder.load_file("createpdf.kv")
         self.theme_cls.primary_palette="DeepPurple"
-        return kv
-
+        return self.kv
+        
     def on_start(self):
-        icons_item={
-            "folder":"Open folders",
-            "upload":"Upload Document"
-
-        }
-        for icon_name in icons_item.keys(): #content_drawer.ids.md_list.add_widget
-            self.root.ids.md_list.add_widget(
-            ItemDrawer(icon=icon_name, text=icons_item[icon_name])
-        )
-
-    def callback(self,button):
-        self.menu.caller=button
-        self.menu.open()
-    def menu_callback(self):
-        self.menu.dismiss()
+        self.menu=MDDropdownMenu(caller=self.kv.ids.plus,width_mult=4)
+        
+        self.menu.items.append({
+            "viewclass":"OneLineListItem",
+            "text":"Upload Document",
+            "callback":self.option_callback
+        })
+        self.menu.items.append({
+            "viewclass":"OneLineListItem",
+            "text":"Upload Image",
+            "callback":self.option_callback
+        })
+        self.menu.items.append({
+            "viewclass":"OneLineListItem",
+            "text":"Create PDF",
+            "callback":self.option_callback
+        })
 
     
+    def option_callback(self,text_of_option):
+        print(text_of_option)
 
-        
 
-    def navigation_draw(self):
-        print("Navigation")
     
     def findfile(self):
         path = str(filechooser.open_file(title="Pick an image or document",path=r"C:\Users\Ninja\OneDrive\Desktop"))
@@ -109,16 +83,3 @@ class CreatePDF(MDApp):
 
 if __name__=="__main__":
     CreatePDF().run()
-
-"""
-GridLayout:
-        cols:1
-        size: root.width,root.height*0.2
-        Button:
-            text:"Add Document"
-            on_press:root.findfile()
-        Button:
-            text:"Documents"
-            on_press:
-                root.manager.transition.direction="left"
-                app.root.current="documents"""
